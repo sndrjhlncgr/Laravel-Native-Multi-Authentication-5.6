50e6bff27ca9c501c4045ed306c764d5be490360
SJCC <sandrocagara20@gmail.com>
2020-05-10 14:32:23 +0800

    fix bugs in factory done!!!!

--- src/database/factories/AdminFactory.php
+++ src/database/factories/AdminFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Admin::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'username' => $faker->name,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
     ];
 });
