347dc9bd9a31bbc1f03cca2a9bf695aa2fc381a6
SJCC <sandrocagara20@gmail.com>
2020-05-29 22:38:32 +0800

    fixing ui in change password administrator

--- src/views/auth/administrator/passwords/change_password.blade.php
+++ src/views/auth/administrator/passwords/change_password.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.administrator.app')
- 
+
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Admin Change password</div>
- 
-                <div class="panel-body">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Change password</strong>
+                </div>
+                <div class="card-body">
                     @if (session('error'))
                         <div class="alert alert-danger">
                             {{ session('error') }}
@@ -20,13 +21,13 @@
                         @endif
                     <form class="form-horizontal" method="POST" action="{{ route('admin.change-password.submit') }}">
                         {{ csrf_field() }}
- 
+    
                         <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
-                            <label for="new-password" class="col-md-4 control-label">Current Password</label>
- 
-                            <div class="col-md-6">
+                            <label for="new-password" class="col-md-12 control-label">Current Password</label>
+    
+                            <div class="col-md-12">
                                 <input id="current-password" type="password" class="form-control" name="current-password" required>
- 
+    
                                 @if ($errors->has('current-password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('current-password') }}</strong>
@@ -34,13 +35,13 @@
                                 @endif
                             </div>
                         </div>
- 
+    
                         <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
-                            <label for="new-password" class="col-md-4 control-label">New Password</label>
- 
-                            <div class="col-md-6">
+                            <label for="new-password" class="col-md-12 control-label">New Password</label>
+    
+                            <div class="col-md-12">
                                 <input id="new-password" type="password" class="form-control" name="new-password" required>
- 
+    
                                 @if ($errors->has('new-password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('new-password') }}</strong>
@@ -48,17 +49,17 @@
                                 @endif
                             </div>
                         </div>
- 
+    
                         <div class="form-group">
-                            <label for="new-password-confirm" class="col-md-4 control-label">Confirm New Password</label>
- 
-                            <div class="col-md-6">
+                            <label for="new-password-confirm" class="col-md-12 control-label">Confirm New Password</label>
+    
+                            <div class="col-md-12">
                                 <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation" required>
                             </div>
                         </div>
- 
+    
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-12 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Change Password
                                 </button>
